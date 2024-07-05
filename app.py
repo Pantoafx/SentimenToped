954bb39d1aa428e9bc3052aabeb8f5d4494a3670
Rahmat Ramadhan <pantoafx@gmail.com>
2024-07-06 05:54:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import re
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
-# Muat model dan vectorizer dari file .pkl
+# Muat model dari file .pkl
 with open('model_svm.pkl', 'rb') as model_file:
     modelsvc_loaded = pickle.load(model_file)
 
@@ -16,17 +16,39 @@ def preprocess_text(text):
     text = stemmer.stem(text)  # Stemming
     return text
 
-# Streamlit app
-st.title('Sentimen Analisis')
+# Halaman Utama
+def main():
+    uhm = 'logo.png'
+    uhm1 = 'logo.png'
+    sidebar_logo = uhm 
+    main_body_logo = uhm1
+    st.logo(sidebar_logo, icon_image=main_body_logo)
+    st.sidebar.markdown('https://handayani.ac.id/')
 
-text_input = st.text_area("Masukkan teks untuk analisis sentimen:")
+    st.markdown("<h1 style='text-align: center; color: #ff6347;'>Analisis Sentimen Ulasan Tokopedia</h1>", unsafe_allow_html=True)
 
-if st.button('Analisis'):
-    if text_input:
-        text_clean = preprocess_text(text_input)
-        text_vector = modelsvc_loaded.named_steps['vectorizer'].transform([text_clean])
-        prediction = modelsvc_loaded.named_steps['classifier'].predict(text_vector)
-        sentiment_label = 'positif' if prediction[0] == 'positif' else 'negatif'
-        st.write(f'Sentimen: {sentiment_label}')
-    else:
-        st.write('Teks tidak boleh kosong')
+    # Input teks dari pengguna
+    user_input = st.text_area('Paste Ulasan Tokopedia disini...')
+
+    # Tombol untuk analisis sentimen
+    if st.button('Analisis Sentimen'):
+        if user_input:
+            # Pra-pemrosesan teks
+            text_clean = preprocess_text(user_input)
+
+            # Transformasi teks dengan model dan prediksi sentimen
+            text_vector = modelsvc_loaded['vectorizer'].transform([text_clean])
+            prediction = modelsvc_loaded['classifier'].predict(text_vector)
+
+            # Tentukan label sentimen
+            sentiment_label = 'positif' if prediction[0] == 'positif' else 'negatif'
+
+            # Menambahkan emoji berdasarkan sentimen
+            emoji = ':smiley:' if sentiment_label == 'positif' else ':persevere:'
+
+            st.write(f'Sentimen : {sentiment_label} {emoji}')
+        else:
+            st.warning('Masukkan teks untuk menganalisis.')
+
+if __name__ == '__main__':
+    main()
