bd54ed3cc2c58b463620198ec8ffd3fdc3bbad6c
Rahmat Ramadhan <pantoafx@gmail.com>
2024-07-06 03:57:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, jsonify, render_template
+import streamlit as st
 import pickle
 import re
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-
-app = Flask(__name__)
 
 # Muat model dan vectorizer dari file .pkl
 with open('model_svm.pkl', 'rb') as model_file:
@@ -18,31 +16,14 @@ def preprocess_text(text):
     text = stemmer.stem(text)  # Stemming
     return text
 
-# Halaman Utama
-@app.route('/')
-def home():
-    return render_template('index.html')  # Render template HTML
+# Streamlit app
+st.title('Sentimen Analisis')
 
-# Endpoint untuk menganalisis sentimen
-@app.route('/analyze', methods=['POST'])
-def analyze_sentiment():
-    data = request.get_json()  # Ambil data dari request
-    text = data.get('text')  # Ambil teks dari data
+text_input = st.text_area("Masukkan teks untuk analisis sentimen:")
 
-    if not text:
-        return jsonify({'error': 'Teks tidak boleh kosong'}), 400
-
-    # Pra-pemrosesan teks
-    text_clean = preprocess_text(text)
-
-    # Transformasi teks dengan vectorizer dan prediksi sentimen
-    text_vector = modelsvc_loaded.named_steps['vectorizer'].transform([text_clean])
-    prediction = modelsvc_loaded.named_steps['classifier'].predict(text_vector)
-
-    # Tentukan label sentimen
-    sentiment_label = 'positif' if prediction[0] == 'positif' else 'negatif'
-
-    return jsonify({'sentiment': sentiment_label})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if st.button('Analisis'):
+    if text_input:
+        text_clean = preprocess_text(text_input)
+        text_vector = modelsvc_loaded.named_steps['vectorizer'].transform([text_clean])
+        prediction = modelsvc_loaded.named_steps['classifier'].predict(text_vector)
+        sentiment_label = 'positif' if prediction[0
